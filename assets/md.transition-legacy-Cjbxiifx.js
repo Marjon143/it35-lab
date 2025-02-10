ddc59a246322300353eaeaeb8d259f25f102022c
Marjon143 <marjonjudilla143@gmail.com>
2025-02-10 13:15:57 +0800

    Updates

--- assets/md.transition-legacy-Cjbxiifx.js
+++ assets/md.transition-legacy-Cjbxiifx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D5uNjcSF.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-CmBCBJB9.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
