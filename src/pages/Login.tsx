7e6cf556e2c5fc31d06657f5d06c921e0c04de62
Mrgumball2004 <deluxerelax4@gmail.com>
2025-02-13 14:13:32 +0800

    added Login Template

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,23 @@
 import { 
+  IonButton,
     IonButtons,
       IonContent, 
       IonHeader, 
       IonMenuButton, 
       IonPage, 
       IonTitle, 
-      IonToolbar 
+      IonToolbar, 
+      useIonRouter
   } from '@ionic/react';
-  
+
   const Login: React.FC = () => {
+    const navigation = useIonRouter();
+
+    const doLogin = () => {
+      navigation.push('/it35-lab/app','forward','replace');
+  }
+  
+  
     return (
       <IonPage>
         <IonHeader>
@@ -20,6 +29,11 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
+
+        <IonButton onClick={() => doLogin()} expand="full">
+                Login
+            </IonButton>
+            
         </IonContent>
       </IonPage>
     );
