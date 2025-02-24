69d32602fb07445d0bced1079c957bdb967f9017
jasiel333 <kaisermeatriderr@gmail.com>
2025-02-24 14:18:24 +0800

    explore ionic framework component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,42 +1,75 @@
+import React from 'react';
 import { 
   IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonInput, 
+  IonMenuButton, 
+  IonPage, 
+  IonText, 
+  IonTitle, 
+  IonToolbar, 
+  IonItem, 
+  IonList, 
+  useIonRouter,
+  IonInputPasswordToggle,
+  IonAvatar,
+  IonLabel
+} from '@ionic/react';
 
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
 
-    const doLogin = () => {
-      navigation.push('/it35-lab/app','forward','replace');
-  }
-  
-  
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-
-        <IonButton onClick={() => doLogin()} expand="full">
-                Login
-            </IonButton>
-            
-        </IonContent>
-      </IonPage>
-    );
+  const doLogin = () => {
+    navigation.push('/it35-lab/app', 'forward', 'replace');
   };
-  
-  export default Login;
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen className="ion-text-center">
+        <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', height: '100%' }}>
+          
+          {}
+          <IonAvatar style={{ marginBottom: '20px' }}>
+            <img alt="Silhouette of a person's head" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNt-Vy_I4LN6JZhvwnJpxoetaJP321ospbf_fxhzPABPv1k67JLDsQbammchNbmtep_H1RVS0-rq9_qqdpO8vYgA" />
+          </IonAvatar>
+
+          <IonList style={{ width: '80%' }}>
+            {}
+            <IonItem>
+              <IonInput labelPlacement="floating" value="hi@ionic.io">
+                <div slot="label">
+                  Username<IonText color="danger">(Required)</IonText>
+                </div>
+              </IonInput>
+            </IonItem>
+
+            {}
+            <IonItem>
+              <IonInput type="password" label="Password" value="NeverGonnaGiveYouUp">
+                <IonInputPasswordToggle slot="end" />
+              </IonInput>
+            </IonItem>
+          </IonList>
+
+          {}
+          <IonButton onClick={() => doLogin()} expand="full" style={{ marginTop: '20px', width: '80%' }}>
+            Login
+          </IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,38 +1,57 @@
+import React from 'react';
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonCard, 
+  IonCardContent, 
+  IonCardHeader, 
+  IonCardSubtitle, 
+  IonCardTitle 
+} from '@ionic/react';
 
+const Feed: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
         <div
           style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
             height: '100%',
+            flexDirection: 'column',
+            gap: '20px', 
           }}
         >
-          Feed
+          <IonCard>
+            <img alt="Silhouette of mountains" src="https://nationaltoday.com/wp-content/uploads/2020/02/doggy-date-night.jpg" />
+            <IonCardHeader>
+              <IonCardTitle>Tanly</IonCardTitle>
+              <IonCardSubtitle>Cutie</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              pagka lovely sa doggy
+            </IonCardContent>
+          </IonCard>
+
+          {}
         </div>
-        
-        </IonContent>
-      </IonPage>
-    );
-  };
-  export default Feed;
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Feed;
