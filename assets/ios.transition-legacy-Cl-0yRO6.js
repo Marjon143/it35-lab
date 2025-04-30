718d1d10772b982d765b6097bfb08e8c8aaa2c47
Cobyestahan <estahanjacobjay@gmail.com>
2025-04-30 17:05:24 +0800

    Updates

--- assets/ios.transition-legacy-Cl-0yRO6.js
+++ assets/ios.transition-legacy-Cl-0yRO6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Q5ca2ZqN.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BPCQfUp4.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
